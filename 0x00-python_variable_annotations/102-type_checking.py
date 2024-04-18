5bfc00d7243db7ac7d989b7ca324b0c43af792c4
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-18 20:15:47 +0100

    Refactor zoom_array function to use List instead of Tuple in 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 ''' Module '''
-from typing import Tuple
+from typing import Tuple, Any, List
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
-    zoomed_in: Tuple[int, ...] = tuple([
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
-    ])
+    ]
     return zoomed_in
 
 
