942b2e6fe85664763e9e592bf6b6cb43c0e0748c
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-18 17:20:06 +0100

    Fix formatting in add function in 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 ''' Module add '''
 
-def add(a:float, b:float) -> float:
+
+def add(a: float, b: float) -> float:
     ''' Add two floats and return the sum as a float '''
     return a + b
