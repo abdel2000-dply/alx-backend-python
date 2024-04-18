74eb1c05840810a5b0f2b6fc0f2fb6c4bdaa083b
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-18 18:19:42 +0100

    Update element_length function signature in 9-element_length.py. adding sequence and iterable typing

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 ''' Element Length Module '''
-from typing import List, Tuple
+from typing import Sequence, Iterable, List, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     ''' Returns a list of tuples containing elements and their length '''
     return [(i, len(i)) for i in lst]
