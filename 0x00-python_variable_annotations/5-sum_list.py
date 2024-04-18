bba17a0e7ef46f9b9c5d7c6a10c37ca9091740a0
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-18 17:38:39 +0100

    Add sum_list function to 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' Sum List Module '''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    ''' Return the sum of a list of floats '''
+    return sum(input_list)
