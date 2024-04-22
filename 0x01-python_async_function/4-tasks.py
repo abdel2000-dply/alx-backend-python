28d2693909b04946c4738dc5255b39bbf0a78f17
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-22 18:20:40 +0100

    Refactor task_wait_n function to use task_wait_random instead of wait_n

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 ''' task_wait_n '''
-# Take the code from wait_n and alter it into a new function task_wait_n.
-# The code is nearly identical to wait_n except task_wait_random is being called.
-
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
