bc3908c47126cc45ad193bf6c512e803276ecc69
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-22 16:48:02 +0100

    Add concurrent coroutines implementation in 1-concurrent_coroutines.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' concurrent coroutines '''
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    ''' spawn wait_random n times with the specified max_delay'''
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
