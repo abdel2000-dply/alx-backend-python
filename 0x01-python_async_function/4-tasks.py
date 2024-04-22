9c50551047113916ed3b031ccd427edd63687dbb
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-22 18:24:35 +0100

    using asyncio.as_copleted instead of .gather

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,4 +8,4 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     ''' spawn wait_random n times with the specified max_delay'''
     delays = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*delays)
+    return [await delay for delay in asyncio.as_completed(delays)]
