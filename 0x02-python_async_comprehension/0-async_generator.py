d4ba8ae04f518e731a5ec059ea1fd24cdc3efc29
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-23 18:26:11 +0100

    Refactor async_generator function to use Generator instead of AsyncGenerator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 ''' Async Generator '''
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     ''' Async Generator '''
     for _ in range(10):
         await asyncio.sleep(1)
