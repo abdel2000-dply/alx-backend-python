ea6bc28ac192696ea0059ca9a914530aa28217a7
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-23 18:41:14 +0100

    Refactor async_comprehension function to use List[float] instead of list[float]

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 ''' Async Comprehensions '''
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     ''' Async Comprehensions '''
     return [n async for n in async_generator()]
