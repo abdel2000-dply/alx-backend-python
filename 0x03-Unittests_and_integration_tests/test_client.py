98a8fd63f6ddcac0a21c1e5d6aea356620d019bf
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-13 15:05:52 +0100

    task 5: Add unit test for GithubOrgClient's public_repos_url method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 '''tests for client.py'''
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 
 
@@ -18,3 +18,11 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         self.assertEqual(client.org, {'payload': True})
         mock_get_json.assert_called_once()
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        '''test public_repos_url method'''
+        payload = {'repos_url': 'https://api.github.com/orgs/github/repos'}
+        mock_org.return_value = payload
+        client = GithubOrgClient('github')
+        self.assertEqual(client._public_repos_url, payload['repos_url'])
