1d8e06840224c5693df95d50725d880a66f822fa
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-13 01:15:04 +0100

    task 3: Add memoize decorator to utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 '''tests for utils.py'''
 import unittest
 from unittest.mock import patch, Mock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 
 
@@ -42,3 +42,24 @@ class TestGetJson(unittest.TestCase):
             mock.return_value = Mock(json=lambda: test_payload)
             self.assertEqual(get_json(test_url), test_payload)
             mock.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    '''tests for memoize'''
+    def test_memoize(self):
+        '''test memoize'''
+        class TestClass:
+            def a_method(self):
+                '''a method'''
+                return 42
+
+            @memoize
+            def a_property(self):
+                '''a property'''
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock:
+            tc = TestClass()
+            self.assertEqual(tc.a_property, 42)
+            self.assertEqual(tc.a_property, 42)
+            mock.assert_called_once()
